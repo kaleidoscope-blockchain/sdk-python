8a99f0e4a8f2d0fd3f5e0034859d79c11d59aacc
arun-babu-kb <arun.babu@kaleidoscope-blockchain.com>
2024-02-19 12:23:51 +0530

    update

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup, find_packages
 
 setup (
-	name		= "witnesschain",
-	version		= "0.1.3",
-	author		= "arun",
-	author_email	= "x@example.com",
-	description	= "A short description of your package",
-	packages	= find_packages(),
-	classifiers	= [
+	name			= "witnesschain",
+	version			= "0.1.4",
+	author			= "arun",
+	author_email		= "x@example.com",
+	description		= "A short description of your package",
+	packages		= find_packages(),
+	install_requires	= ["requests", "json"],
+	classifiers		= [
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 	],
-	url		= "https://github.com/iudx/iudx-python-sdk",
-	python_requires	= ">=3.6",
+	url			= "https://github.com/kaleidoscope-blockchain/sdk-python",
+	python_requires		= ">=3.6",
 )
