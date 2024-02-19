47279b0f039f62aa39a7e7a446217a3a4d4eb9e9
arun-babu-kb <arun.babu@kaleidoscope-blockchain.com>
2024-02-19 12:05:11 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,8 @@
+import witnesschain
+
+r = witnesschain.trace ({
+  "chainId"         : "84532",
+  "transactionHash" : "0x55625f12e4581ffcedd8f3cf76d05e01c0a40e82a510c9eb03a28bd87166b698",
+})
+
+print(r)
